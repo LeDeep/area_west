cdbda5f71a32e897766f5c561184f6ddc886eb74
Deep <smarreddy1@gmail.com>
2013-07-18 10:18:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AreaWest::Application.config.secret_token = 'd93cd1c69d9e9af3b81c026c9cf269eaf047d6799bc200aaadc1ef6622a1f2521bcfd61be353b973426ba345498f8254c688e3c391a42a898638601374f44839'
