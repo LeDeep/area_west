4b3c7249e29cdb39da2b8c5035e95226ac0af231
Deep <smarreddy1@gmail.com>
2013-08-01 21:35:40 -0700

    update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -16,7 +21,8 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-
+  config.mock_with :rspec
+  config.include Rails.application.routes.url_helpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -35,4 +41,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+end
+
+Spork.each_run do 
+  # This code will be run each time you run your specs.
+
 end
